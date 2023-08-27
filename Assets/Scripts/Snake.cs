823a62a6ddc6205491afc9707ca29461132297a0
Viktor <git@vgsoftware.com>
2023-08-27 22:31:59 +0200

    Worked on menus and snake speed

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -13,6 +13,7 @@ namespace Assets.Scripts
 		{
 			None,
 			Menu,
+			Countdown,
 			Running,
 			Pause,
 			GameOver
@@ -23,15 +24,19 @@ namespace Assets.Scripts
 		private List<Transform> _segments = new List<Transform>();
 		private int _score;
 		private GameState _state;
+		private float _countdown;
+		private float _speedCounter;
 
 		public Transform segmentPrefab;
 		public int startSize = 4;
 		public TextMeshProUGUI playerScoreText;
 		public GameObject mainMenu;
+		public GameObject countdownMenu;
+		public TextMeshProUGUI coundownText;
+		public float speed = 0.16f;
 
 		private void NewGame()
 		{
-			mainMenu.SetActive(false);
 			for (int i = 1; i < _segments.Count; i++)
 			{
 				Destroy(_segments[i].gameObject);
@@ -45,13 +50,13 @@ namespace Assets.Scripts
 			{
 				_segments.Add(Instantiate(segmentPrefab));
 			}
-			_state = GameState.Running;
+			State = GameState.Countdown;
 		}
 
 		private void Awake()
 		{
 			_inputControls = new SnakeControls();
-			_state = GameState.Menu;
+			State = GameState.Menu;
 		}
 
 		private void OnEnable()
@@ -68,7 +73,7 @@ namespace Assets.Scripts
 
 		private void Move_performed(InputAction.CallbackContext context)
 		{
-			if (_state == GameState.Running)
+			if (State == GameState.Running)
 			{
 				var newDirection = context.ReadValue<Vector2>();
 				var x = Mathf.RoundToInt(newDirection.x);
@@ -85,31 +90,110 @@ namespace Assets.Scripts
 			}
 		}
 
+		private void Update()
+		{
+			if (State == GameState.Countdown)
+			{
+				_countdown -= Time.deltaTime;
+				if(_countdown > 0)
+				{
+					coundownText.text = Mathf.CeilToInt(_countdown * 2).ToString();
+				}
+				else
+				{
+					coundownText.text = "GO!";
+					State = GameState.Running;
+				}
+			}
+		}
+
 		private void FixedUpdate()
 		{
-			if(_state == GameState.Running)
+			if(State == GameState.Running)
 			{
-				for (int i = _segments.Count - 1; i > 0; i--)
+				_speedCounter += Time.fixedDeltaTime;
+				if(_speedCounter > speed)
 				{
-					_segments[i].position = _segments[i - 1].position;
+					_speedCounter = 0;
+					for (int i = _segments.Count - 1; i > 0; i--)
+					{
+						_segments[i].position = _segments[i - 1].position;
+					}
+					var x = Mathf.RoundToInt(transform.position.x) + Mathf.RoundToInt(_direction.x);
+					var y = Mathf.RoundToInt(transform.position.y) + Mathf.RoundToInt(_direction.y);
+					transform.position = new Vector3(x, y, transform.position.z);
 				}
-				var x = Mathf.RoundToInt(transform.position.x) + Mathf.RoundToInt(_direction.x);
-				var y = Mathf.RoundToInt(transform.position.y) + Mathf.RoundToInt(_direction.y);
-				transform.position = new Vector3(x, y, transform.position.z);
+			}
+		}
+
+		private void HideCountdownMenu()
+		{
+			countdownMenu.SetActive(false);
+		}
+
+		private GameState State
+		{
+			get => _state;
+			set
+			{
+				if (_state == value)
+				{
+					return;
+				}
+				_state = value;
+				if(_state==GameState.Menu)
+				{
+					mainMenu.SetActive(true);
+					countdownMenu.SetActive(false);
+				}
+				else if (_state == GameState.Running)
+				{
+					mainMenu.SetActive(false);
+					Invoke(nameof(HideCountdownMenu), 0.5f);
+				}
+				else if (_state == GameState.Countdown)
+				{
+					mainMenu.SetActive(false);
+					countdownMenu.SetActive(true);
+					_countdown = 1.5f;
+				}
+				else if (_state == GameState.GameOver)
+				{
+					mainMenu.SetActive(true);
+					countdownMenu.SetActive(false);
+				}
 			}
 		}
 
 		private void OnTriggerEnter2D(Collider2D collision)
 		{
-			if (collision.tag == "Food")
+			if (State == GameState.Running)
 			{
-				_score++;
-				playerScoreText.text = _score.ToString();
-				Grow();
-			}
-			else if (collision.tag == "Obsticle")
-			{
-				NewGame();
+				if (collision.tag == "Food")
+				{
+					_score++;
+					if (speed > 0.02f)
+					{
+						if (_score == 1 || _score == 3 || _score == 6 || _score == 10)
+						{
+							speed -= 0.01f;
+						}
+						else if (_score == 15 || _score == 20 || _score == 25 || _score == 30)
+						{
+							speed -= 0.01f;
+						}
+						else
+						{
+							speed -= 0.001f;
+						}
+					}
+					playerScoreText.text = _score.ToString();
+					Grow();
+				}
+				else if (collision.tag == "Obsticle")
+				{
+					State = GameState.GameOver;
+				}
 			}
 		}
 
