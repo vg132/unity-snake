3d3a8c55e9fa53a4d03cabfe5dc7caf9c0e1c6d6
Viktor <git@vgsoftware.com>
2023-08-29 22:49:38 +0200

    Performance fix for highscore

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -4,18 +4,24 @@ namespace Assets.Scripts
 {
 	public static class Settings
 	{
+		private static int? _highScore = null;
 		public static int HighScore
 		{
 			get
 			{
-				return PlayerPrefs.GetInt(nameof(HighScore), 0);
+				if(_highScore == null)
+				{
+					_highScore= PlayerPrefs.GetInt(nameof(HighScore), 0);
+				}
+				return _highScore.Value;
 			}
 			set
 			{
-				if(value>HighScore)
+				if (value > HighScore)
 				{
 					PlayerPrefs.SetInt(nameof(HighScore), value);
 					PlayerPrefs.Save();
+					_highScore = value;
 				}
 			}
 		}
